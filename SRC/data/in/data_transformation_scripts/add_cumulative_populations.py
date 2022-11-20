fc14bf43770a0ddf57dcbe47986705d30e71799e
Brody Smith <bsmit272@uwo.ca>
2022-11-20 15:58:36 -0500

    Add street and population data

---
+++ SRC/data/in/data_transformation_scripts/add_cumulative_populations.py
@@ -0,0 +1,30 @@
+# find totals of NL and NS population, convert to %,
+# then add cumulative pop
+
+import csv
+
+f = open('nova-scotia-population.csv')
+fwrite = open('nova-scotia-population-modified.csv', 'w')
+fw = csv.writer(fwrite, dialect="unix")
+copy = []
+
+sum = 0
+for line in f: 
+    print("line: " + line)
+    city, pop = line.split(',')
+    copy.append([city, pop])
+    sum += int(pop)
+
+
+f.close()
+second = lambda l : l[1]
+copy.sort(key=second, reverse=True) # sort from asc to desc
+
+c_sum = 0.0
+for line in copy:
+    local_sum = float(line[1]) / float(sum) * 100.0
+    c_sum += local_sum
+    fw.writerow([line[0], line[1], local_sum, c_sum])
+
+
+fwrite.close()
