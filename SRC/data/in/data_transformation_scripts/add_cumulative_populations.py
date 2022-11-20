8b960b6743219f2454ec1b48c90edc52472e9fee
Brody Smith <bsmit272@uwo.ca>
2022-11-20 17:08:37 -0500

    Fix data sorting for populations, add province data
    Province data also has cumsum, which will be used for random province selection

--- SRC/data/in/data_transformation_scripts/add_cumulative_populations.py
+++ SRC/data/in/data_transformation_scripts/add_cumulative_populations.py
@@ -3,28 +3,27 @@
 
 import csv
 
-f = open('nova-scotia-population.csv')
-fwrite = open('nova-scotia-population-modified.csv', 'w')
+f = open('../source/newfoundland-population.csv')
+fwrite = open('../newfoundland-population-modified.csv', 'w')
 fw = csv.writer(fwrite, dialect="unix")
 copy = []
 
 sum = 0
 for line in f: 
-    print("line: " + line)
     city, pop = line.split(',')
     copy.append([city, pop])
     sum += int(pop)
 
-
+print(sum)
 f.close()
-second = lambda l : l[1]
+second = lambda l : int(l[1])
 copy.sort(key=second, reverse=True) # sort from asc to desc
 
 c_sum = 0.0
 for line in copy:
     local_sum = float(line[1]) / float(sum) * 100.0
     c_sum += local_sum
-    fw.writerow([line[0], line[1], local_sum, c_sum])
+    fw.writerow([line[0], line[1][:-1], local_sum, c_sum])
 
 
 fwrite.close()
